364f2010e4666d7988c0cef15f09b4d743def625
Ete Remi <remi.ete@desy.de>
2019-06-04 20:25:05 +0200

    Changed compression interface to take the output buffer in argument

--- sio/include/sio/compression/zlib.h
+++ sio/include/sio/compression/zlib.h
@@ -16,44 +16,46 @@ namespace sio {
     ~zlib_compression() = default ;
 
     /**
-     *  @brief  Uncompress the buffer and return a new buffer
+     *  @brief  Uncompress the buffer and return a new buffer (reference).
+     *          The uncpmpressed buffer must have been resized correctly 
+     *          before calling this function.
      * 
      *  @param  inbuf the input buffer to uncompress
-     *  @param  out_len the uncompressed buffer length (read out from the record header)
+     *  @param  outbuf the uncompressed buffer to receive
      */
-    buffer uncompress( const buffer_span &inbuf, std::size_t out_len ) ;
+    void uncompress( const buffer_span &inbuf, buffer &outbuf ) ;
     
     /**
      *  @brief  Compress the buffer and return a new buffer
      * 
      *  @param  inbuf the input buffer to compress
+     *  @param  outbuf the output buffer to receive
      *  @param  comp_level the compression level
      */
-    buffer compress( const buffer_span &inbuf, int comp_level = Z_DEFAULT_COMPRESSION ) ;
+    void compress( const buffer_span &inbuf, buffer &outbuf, int comp_level = Z_DEFAULT_COMPRESSION ) ;
   };
   
   //--------------------------------------------------------------------------
   //--------------------------------------------------------------------------
   
-  buffer zlib_compression::uncompress( const buffer_span &inbuf, std::size_t out_len ) {
+  void zlib_compression::uncompress( const buffer_span &inbuf, buffer &outbuf ) {
     if( not inbuf.valid() ) {
       SIO_THROW( sio::error_code::invalid_argument, "Buffer is not valid" ) ;
     }
     // zlib uncompress requires to pass an address of uLongf type ...
-    uLongf outsize = out_len ;
-    buffer outbuf( out_len ) ;
+    uLongf outsize = outbuf.size() ;
     auto zstat = ::uncompress( (Bytef*)outbuf.data(), &outsize, (const Bytef*)inbuf.data(), inbuf.size() ) ;
     if( Z_OK != zstat ) {
       std::stringstream ss ;
       ss << "Zlib uncompression failed with status " << zstat ;
       SIO_THROW( sio::error_code::compress_error, ss.str() ) ;
     }
-    return outbuf ;
+    SIO_DEBUG( "ZLIB uncompress OK!" ) ;
   }
   
   //--------------------------------------------------------------------------
   
-  buffer zlib_compression::compress( const buffer_span &inbuf, int comp_level ) {
+  void zlib_compression::compress( const buffer_span &inbuf, buffer &outbuf, int comp_level ) {
     if( not inbuf.valid() ) {
       SIO_THROW( sio::error_code::invalid_argument, "Buffer is not valid" ) ;
     }
@@ -64,7 +66,9 @@ namespace sio {
     // After compression, the real output size is returned,
     // this is why the buffer is resized after calling compress2().
     auto comp_bound = ::compressBound( inbuf.size() ) ;
-    buffer outbuf( comp_bound ) ;
+    if( outbuf.size() < comp_bound ) {
+      outbuf.resize( comp_bound ) ;
+    }
     auto zstat = ::compress2( (Bytef*)outbuf.data(), &comp_bound, (const Bytef*)inbuf.data(), inbuf.size(), comp_level ) ;
     if( Z_OK != zstat ) {
       std::stringstream ss ;
@@ -72,7 +76,7 @@ namespace sio {
       SIO_THROW( sio::error_code::compress_error, ss.str() ) ;
     }
     outbuf.resize( comp_bound ) ;
-    return outbuf ;
+    SIO_DEBUG( "ZLIB compress OK!" ) ;
   }
 }
 
