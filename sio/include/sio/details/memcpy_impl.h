e22215d51d7e6619e4c68027787c129bd1b24c2b
Ete Remi <remi.ete@desy.de>
2019-06-05 14:04:22 +0200

    Added back memcpy_impl.h

---
+++ sio/include/sio/details/memcpy_impl.h
@@ -0,0 +1,27 @@
+
+
+namespace sio {
+  
+  void memcpy::reverse_copy( const unsigned char *const from, unsigned char *dest, std::size_t size, std::size_t count ) {
+  dest += size;
+  std::size_t jump = size << 1;
+  const unsigned char *lfrom = from ;
+  for( std::size_t icnt = 0; icnt < count; icnt++ ) {
+    for( std::size_t ibyt = 0; ibyt < size; ibyt++  ) {
+      *--dest = *lfrom++;
+    }
+    dest += jump;
+  }
+}
+
+//--------------------------------------------------------------------------
+
+void memcpy_helper::copy( const unsigned char *const from, unsigned char *dest, std::size_t size, std::size_t count ) {
+#ifdef SIO_BIG_ENDIAN
+  std::memcpy( dest, from, size * count ) ;
+#else
+  sio::memcpy::reverse_copy( from, dest, size, count ) ;
+#endif
+}
+  
+}
