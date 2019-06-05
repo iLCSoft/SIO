cab25915c9f6bc2b28bcf04ebc408d4ff1e01262
Ete Remi <remi.ete@desy.de>
2019-06-05 17:28:55 +0200

    Added write_device class

--- sio/include/sio/details/io_device_impl.h
+++ sio/include/sio/details/io_device_impl.h
@@ -30,6 +30,9 @@ namespace sio {
   //--------------------------------------------------------------------------
 
   inline void read_device::seek( cursor_type pos ) {
+    if( pos > _buffer.size() ) {
+      SIO_THROW( sio::error_code::out_of_range, "Can't seek device cursor: out of range!" ) ;
+    }
     _cursor = pos ;
   }
 
@@ -87,6 +90,88 @@ namespace sio {
     sio::api::read_relocation( _pointed_at, _pointer_to ) ;
     _pointer_to.clear() ;
     _pointed_at.clear() ;
+  }
+  
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+  
+  void write_device::set_buffer( buffer&& buf ) {
+    _buffer = std::move( buf ) ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  buffer write_device::take_buffer() {
+    return std::move( _buffer ) ;
+  }
+  
+  //--------------------------------------------------------------------------
+
+  write_device::cursor_type write_device::position() const {
+    return _cursor ;
+  }
+  
+  //--------------------------------------------------------------------------
+
+  void write_device::seek( cursor_type pos ) {
+    if( pos > _buffer.size() ) {
+      SIO_THROW( sio::error_code::out_of_range, "Can't seek device cursor: out of range!" ) ;
+    }
+    _cursor = pos ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  void write_device::data( const T &var ) {
+    data( &var, 1 ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  void write_device::data( const T *const var, size_type count ) {
+    _cursor += sio::api::write( _buffer, var, _cursor, count ) ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  void write_device::pointer_to( ptr_type *ptr ) {
+    // Write.  Keep a record of the "match" quantity (i.e. the value of the
+    // pointer (which may be different lengths on different machines!)) and
+    // the current offset in the output buffer.  Put a placeholder in the
+    // output buffer (it will be overwritten at the "output relocation" stage).
+    // 
+    // Placeholder value for 'pointer to'
+    unsigned int SIO_pntr = 0x00000000 ;
+    // ptr is really a pointer-to-a-pointer.  This routine is most interested
+    // in the value of *xfer when treated as a pointer.  C++ tends to object
+    // to this as being 'not type safe'.  To keep the compiler happy (and purists
+    // miserable), do one 'reinterpret_cast' immediately to make later code
+    // easier to read.
+    // Indirect ptr (actually **ptr)
+    void *ifer = reinterpret_cast<void *>(*ptr) ;
+    // Ignore NULL pointers.  These are always recorded in the buffer with a
+    // zero match word (and are treated specially when read back).  There's no
+    // point in putting useless information in the maps.
+    if( nullptr != ifer ) {
+      sio::pointer_to_map::value_type entry = { ifer, reinterpret_cast<void *>( _buffer.ptr(_cursor) - _buffer.data() ) } ;
+      _pointer_to.insert( entry ) ;
+    }
+    data( SIO_pntr ) ;
+  }
+  
+  //--------------------------------------------------------------------------
+
+  void write_device::pointed_at( ptr_type *ptr ) {
+    // Write.  Save the memory location of this object along with the offset
+    // in the output buffer where the generated match quantity must go.  Put
+    // a placeholder in the output buffer (it will be overwritten at the "output
+    // relocation" stage).
+    unsigned int SIO_ptag = 0xffffffff ;
+    sio::pointed_at_map::value_type entry = { ptr, reinterpret_cast<void *>( _buffer.ptr(_cursor) - _buffer.data() ) } ;
+    _pointed_at.insert( entry ) ;
+    data( SIO_ptag ) ;
   }
 
 }
--- sio/include/sio/io_device.h
+++ sio/include/sio/io_device.h
@@ -131,6 +131,126 @@ namespace sio {
     ///< The map of pointer "pointer to"
     pointer_to_map      _pointer_to {} ;
   };
+  
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+  
+  /**
+   *  @brief  write_device class.
+   *
+   *  Holds a buffer and provide a api to write data sequentially 
+   *  to this buffer by using a cursor
+   */
+  class write_device {
+  public:
+    using cursor_type = std::size_t ;
+    using size_type = std::size_t ;
+
+  public:
+    /// Default constructor
+    write_device() = default ;
+    /// Default copy constructor
+    write_device( const write_device & ) = default ;
+    /// Default move constructor
+    write_device( write_device && ) = default ;
+    /// Default assignement operator
+    write_device& operator=( const write_device & ) = default ;
+    /// Default move assignement operator
+    write_device& operator=( write_device && ) = default ;
+    /// Default destructor
+    ~write_device() = default ;
+    
+    /**
+     *  @name Buffer
+     */
+    ///{@
+    /**
+     *  @brief  Set the buffer to use (move)
+     * 
+     *  @param  buf the buffer to use
+     */
+    void set_buffer( buffer&& buf ) ;
+    
+    /**
+     *  @brief  Take out the buffer from the device.
+     *          The buffer in the device becomes invalid
+     */
+    buffer take_buffer() ;
+    ///@}
+    
+    /**
+     *  @name Cursor
+     */
+    ///{@
+    /**
+     *  @brief  Get the cursor position
+     */
+    cursor_type position() const ;
+    
+    /**
+     *  @brief  Seek the cursor at a given position
+     * 
+     *  @param  pos the new cursor position
+     */
+    void seek( cursor_type pos ) ;
+    ///@}
+
+    /**
+     *  @name I/O operations
+     */
+    ///{@
+    /**
+     *  @brief  Write out a variable to the buffer. Move the cursor accordingly
+     * 
+     *  @param  var the variable to write
+     */
+    template <typename T>
+    void data( const T &var ) ;
+
+    /**
+     *  @brief  Write out an array of variables to the buffer. Move the cursor accordingly
+     * 
+     *  @param  var the address of the array
+     *  @param  count the number of element to write out
+     */
+    template <typename T>
+    void data( const T *const var, size_type count ) ;
+    
+    /**
+     *  @brief  Write out a "pointer to" pointer to the buffer.
+     *          A new entry is created for a future relocation.
+     *          The pointer relocation is performed at the end of record writting
+     *          
+     *  @param  ptr the address to register
+     */
+    void pointer_to( ptr_type *ptr ) ;
+    
+    /**
+     *  @brief  Write out a "pointed at" pointer to the buffer.
+     *          A new entry is created for a future relocation.
+     *          The pointer relocation is performed at the end of record writting
+     *          
+     *  @param  ptr the address to register
+     */
+    void pointed_at( ptr_type *ptr ) ;
+    
+    /**
+     *  @brief  Perform the pointer relocation after the whole record has
+     *          been written. The pointers are relocated and the pointer maps
+     *          are cleared
+     */
+    void pointer_relocation() ;
+    
+  private:
+    ///< The buffer in which to write
+    buffer              _buffer {32*sio::kbyte*sio::kbyte} ;
+    ///< The device cursor
+    cursor_type         _cursor {0} ;
+    ///< The map of pointer "pointed at"
+    pointed_at_map      _pointed_at {} ;
+    ///< The map of pointer "pointer to"
+    pointer_to_map      _pointer_to {} ;
+  };
 
 }
 
