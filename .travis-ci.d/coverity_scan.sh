16eb4cd44f3356fb1cbcf44b054160e2070a4361
Ete Remi <remi.ete@desy.de>
2019-06-12 11:51:14 +0200

    Compile sio examples in travis and coverity scan

--- .travis-ci.d/coverity_scan.sh
+++ .travis-ci.d/coverity_scan.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake .. && \
+cmake -DSIO_EXAMPLES=ON .. && \
 export PATH=/Package/cov-analysis-linux64/bin:$PATH && \
 cov-build --dir cov-int make -j2 && \
 tar czvf myproject.tgz cov-int
