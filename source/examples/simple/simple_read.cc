5bf6b7e6fac17bac7129f127e65559a839c725a4
Remi Ete <eteremi@flc03.desy.de>
2019-07-10 14:58:09 +0200

    Added printout for unit tests

--- source/examples/simple/simple_read.cc
+++ source/examples/simple/simple_read.cc
@@ -70,6 +70,8 @@ int main( int argc, char **argv ) {
     
     /// A bit of cleanup and we are done !
     stream.close() ;
+    
+    std::cout << "Read sio file " << fname << std::endl ;
   }
   catch( sio::exception &e ) {
     std::cout << "Caught sio exception :\n" << e.what() << std::endl ;
