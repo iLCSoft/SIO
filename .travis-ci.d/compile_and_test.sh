92b56e079fde72e73b6da6d3776b82d30dae8271
Remi Ete <remi.ete@gmail.com>
2020-09-24 10:19:41 +0200

    CI test with valgrind

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -8,5 +8,9 @@ mkdir build
 cd build
 cmake -GNinja -DSIO_BUILTIN_ZLIB=$BUILTIN_ZLIB -DSIO_EXAMPLES=ON -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
 ninja  -k 0 && \
-ninja install && \
-ctest --output-on-failure --verbose
+ninja install 
+#&& \
+#ctest --output-on-failure --verbose
+
+./bin/relocation_write
+valgrind ./bin/relocation_read relocation.sio
