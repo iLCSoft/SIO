48f2006b2229cc6ca0247712f952ba804c7771c2
tmadlener <thomas.madlener@desy.de>
2020-09-30 13:51:14 +0200

    Add --detailed flag to sio-dump and remove sio-dump-detailed

--- source/main/sio-dump.cc
+++ source/main/sio-dump.cc
@@ -6,42 +6,105 @@
 
 // -- std headers
 #include <limits>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+
+
+constexpr const char *USAGE = R"(Usage: sio-dump [-d/--detailed] [-n NRECORDS] [-m SKIPRECORDS] siofile)";
+
+constexpr const char *HELP = R"(Dump SIO record from file to console
+
+Positional arguments:
+  siofile:         The file from which the records should be dumped
+
+Optional arguments:
+  -h, --help       Show the help message and exit
+  -d, --detailed   Dump detailed (i.e. unpacked) records from the file
+  -n NRECORDS      Only dump NRECORDS records from the file
+  -m SKIPRECORDS   Skip the first SKIPRECORDS from dumping
+)";
+
+/**
+ * @brief Check if either a short form or a long form of the option is in the
+ * list of arguments.
+ */
+bool has_option(std::vector<std::string>& args, const char* opt_s, const char* opt_l) {
+  const auto it = std::find_if(args.cbegin(), args.cend(),
+                               [opt_s, opt_l] (const std::string& arg) {
+                                 return arg == opt_s || arg == opt_l;
+                               });
+
+   if (it != args.cend()) {
+     args.erase(it);
+     return true;
+   }
+
+   return false;
+}
+
+/**
+ * @brief Check if a given option is in the list of arguments and get the
+ * corresponding value or a default value if the argument is not present.
+ *
+ * Print the usage message and exit if the conversion fails or if the value to
+ * the argument is missing.
+ */
+int option_val(std::vector<std::string>& args, const char* opt_s, int def_val=-1) {
+  const auto it = std::find_if(args.cbegin(), args.cend(), [opt_s] (const std::string& arg) {
+    return arg == opt_s;
+  });
+
+  if (it != args.cend()) {
+    const auto value_index = std::distance(args.cbegin(), it) + 1;
+    // make sure that the value follows the argument
+    if ((int)args.size() < value_index) { // silence the Wsign-compare
+      std::cout << USAGE << std::endl;
+      std::exit(1);
+    }
+    try {
+      const int value = std::stoi(args[value_index]);
+      args.erase(it, it + 1);
+      return value;
+    } catch (const std::invalid_argument&) {
+      std::cerr << "Cannot convert \'" << args[value_index] << "\' to int for argument \'" << opt_s << "\'\n\n";
+      std::cout << USAGE << std::endl;
+      std::exit(1);
+    }
+  }
+
+  return def_val;
+}
 
 /**
  *  @brief  Utility in sio to dump all records from a file on disk
  */
 int main( int argc, char **argv ) {
-  
-  if( (argc < 2) or (argc > 4) ) {
-    std::cout << "Dump SIO record from file to console" << std::endl ;
-    std::cout << "Usage: " << std::endl ;
-    std::cout << "- Dump all records from the file:" << std::endl ;
-    std::cout << "     sio-dump <file-name>" << std::endl ;
-    std::cout << "- Dump the n first records from the file:" << std::endl ;
-    std::cout << "     sio-dump <file-name> <n>" << std::endl ;
-    std::cout << "- Skip the m first record and dump the n next records from the file:" << std::endl ;
-    std::cout << "     sio-dump <file-name> <m> <n>" << std::endl ;
-    return 1 ;
+  std::vector<std::string> args(argv + 1, argv + argc);
+
+  const int count = option_val(args, "-n", std::numeric_limits<int>::max());
+  const int skip = option_val(args, "-m", 0);
+  const bool detailed = has_option(args, "-d", "--detailed");
+
+  if (has_option(args, "-h", "--help")) {
+    std::cout << USAGE << "\n\n";
+    std::cout << HELP << std::endl;
+    return 0;
   }
-  
-  const std::string fname = argv[1] ;
-  unsigned int count = std::numeric_limits<unsigned int>::max() ;
-  unsigned int skip = 0 ;
-  
-  if( argc > 3 ) {
-    skip = atoi(argv[2]) ;
-    count = atoi(argv[3]) ;
-    std::cout << "Will skip " << skip << " records and dump the " << count << " next ones ..." << std::endl ;
+
+  if (args.size() < 1) {
+    std::cout << USAGE << std::endl;
+    return 0;
   }
-  else if( argc > 2 ) {
-    count = atoi(argv[2]) ;
-    std::cout << "Will dump only " << count << " records ..." << std::endl ;
-  }
-  
+
+  const auto& fname = args.back();
+
   sio::ifstream file;
   file.open( fname , std::ios::in | std::ios::binary ) ;
   try {
-    sio::api::dump_records( file, skip, count, false ) ;
+    sio::api::dump_records( file, skip, count, detailed) ;
   } catch( const sio::exception &e ) {
     switch( e.code() ) {
       case sio::error_code::not_open:
